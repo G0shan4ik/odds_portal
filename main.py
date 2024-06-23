4f52fa8e4d7dc179490753e4e621edb41b9f464e
Egor <egorkononec64@gmail.com>
2024-06-24 00:40:28 +0300

    обавил вмчу с уведами остановки/запуска бота

--- main.py
+++ main.py
@@ -4,16 +4,39 @@ import sys
 import logging
 
 from aiogram.methods import DeleteWebhook
+from aiogram.utils.markdown import hbold
 
-from package.database import init
+from package.database import init, Users
 from package.odd_bot.core import bot_, dp
 from package.parsers.main_parser import schedule
+
+
+async def on_startup(dispatcher):
+    for item in Users.select():
+        await bot_.send_message(
+            chat_id=item.user_id,
+            text=hbold('Бот запущен! 🥲')
+        )
+
+
+async def on_shutdown(dispatcher):
+    for item in Users.select():
+        await bot_.send_message(
+            chat_id=item.user_id,
+            text=hbold('Бот остановлен 😥, дла проведения технических работ!')
+        )
 
 
 async def main():
     await bot_(DeleteWebhook(drop_pending_updates=True))
     init()
-    await asyncio.gather(dp.start_polling(bot_), schedule())
+
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    await asyncio.gather(
+        dp.start_polling(bot_),
+        schedule()
+    )
 
 
 def start_dev():
